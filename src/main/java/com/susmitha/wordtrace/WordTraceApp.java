0a3b446de56109bc584e4d92d6d374f9682c8c7c
Susmitha Mereddy <susmitha.mereddy@theelixrlabs.com>
2025-03-07 15:39:28 +0530

    File Input

--- src/main/java/com/susmitha/wordtrace/WordTraceApp.java
+++ src/main/java/com/susmitha/wordtrace/WordTraceApp.java
@@ -1,9 +1,34 @@
 package com.susmitha.wordtrace;
-
+import java.util.Scanner;
+import java.io.File;
 public class WordTraceApp {
-    public static void main(String[] args){
-        System.out.println("Processing");
+    public static void main(String[] args) {
+        //Scanner sc=new Scanner(System.in)
+
+       if(args.length!=2){
+           System.out.println(args.length<2 ? "Please provide both the file path and the word to be sedarched ":"Please provide only the file path and the word to be searched");
+           return;
+       }
+
+       String filePath=args[0];
+       String search=args[1];
+
+       File file=new File(filePath);
+       if(file.exists() && file.isFile()){
+           if(filePath.endsWith(".txt")||filePath.endsWith(".json")) {
+               System.out.println("File Path is correct");
+               System.out.println("Searching for word ? :" + search);
+           }
+           else{
+               System.out.println("Invalid file type. only .txt and .json files are allowed");
+           }
+       }
+       else{
+           System.out.println("File path is incorrect or file doesnot exist");
+       }
+
     }
+
 
 }
 
