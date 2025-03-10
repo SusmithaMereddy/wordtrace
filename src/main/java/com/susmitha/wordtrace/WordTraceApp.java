9bd3adf32774fe647544dd93ef5545c275e20ffa
susmitha.mereddy <mereddysusmitha2002@gmail.com>
2025-03-10 20:58:23 +0530

    Fixed file type validation logic in WordTraceApp and WordTraceConstants

--- src/main/java/com/susmitha/wordtrace/WordTraceApp.java
+++ src/main/java/com/susmitha/wordtrace/WordTraceApp.java
@@ -1,34 +1,32 @@
+
 package com.susmitha.wordtrace;
-import java.util.Scanner;
 import java.io.File;
+
 public class WordTraceApp {
     public static void main(String[] args) {
-        //Scanner sc=new Scanner(System.in)
-
-       if(args.length!=2){
-           System.out.println(args.length<2 ? "Please provide both the file path and the word to be sedarched ":"Please provide only the file path and the word to be searched");
-           return;
-       }
-
-       String filePath=args[0];
-       String search=args[1];
-
-       File file=new File(filePath);
-       if(file.exists() && file.isFile()){
-           if(filePath.endsWith(".txt")||filePath.endsWith(".json")) {
-               System.out.println("File Path is correct");
-               System.out.println("Searching for word ? :" + search);
-           }
-           else{
-               System.out.println("Invalid file type. only .txt and .json files are allowed");
-           }
-       }
-       else{
-           System.out.println("File path is incorrect or file doesnot exist");
-       }
-
+        if (args.length < 2) {
+            System.out.println(WordTraceConstants.Error_InsufficientArguments);
+            return;
+        }
+        else if(args.length > 2){
+            System.out.println(WordTraceConstants.Error_MoreArguments);
+            return;
+        }
+        String inputFilePath = args[0];
+        String searchWord = args[1];
+        System.out.println(WordTraceConstants.Message_InputFilePath+" : " +inputFilePath);
+        System.out.println(WordTraceConstants.Message_SearchWord+ " : "+searchWord);
+        File file = new File(inputFilePath);
+        if (!file.exists() || !file.isFile()) {
+            System.out.println(WordTraceConstants.Error_InvalidFile);
+            return;
+        }
+        if (!inputFilePath.endsWith(WordTraceConstants.File_Extension_txt) && !inputFilePath.endsWith(WordTraceConstants.File_Extension_json)) {
+            System.out.println(WordTraceConstants.Error_InvalidFileType);
+            return;
+        }
+        System.out.println(WordTraceConstants.Message_FilePathCorrect);
+        System.out.println(WordTraceConstants.Message_Searching+" "+ searchWord);
+        System.out.println(WordTraceConstants.Message_Processing);
     }
-
-
 }
-
