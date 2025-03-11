7de09775f6da5806625f31ceb388d0823752c3c6
susmitha.mereddy <mereddysusmitha2002@gmail.com>
2025-03-11 14:01:06 +0530

    added only single functionality to single class

--- src/main/java/com/susmitha/wordtrace/WordTraceApp.java
+++ src/main/java/com/susmitha/wordtrace/WordTraceApp.java
@@ -1,30 +1,16 @@
 package com.susmitha.wordtrace;
-import java.io.File;
+import com.susmitha.wordtraceconstants.WordTraceConstants;
 public class WordTraceApp {
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println(WordTraceConstants.Error_InsufficientArguments);
-            return;
-        }
-        else if(args.length > 2){
-            System.out.println(WordTraceConstants.Error_MoreArguments);
+        String validationMessage = InputValidator.validateInputs(args);
+        if(!validationMessage.isEmpty()){
+            System.out.println(validationMessage);
             return;
         }
         String inputFilePath = args[0];
         String searchWord = args[1];
-        System.out.println(WordTraceConstants.Message_InputFilePath+" : " +inputFilePath);
-        System.out.println(WordTraceConstants.Message_SearchWord+ " : "+searchWord);
-        File file = new File(inputFilePath);
-        if (!file.exists() || !file.isFile()) {
-            System.out.println(WordTraceConstants.Error_InvalidFile);
-            return;
-        }
-        if (!inputFilePath.endsWith(WordTraceConstants.File_Extension_txt) && !inputFilePath.endsWith(WordTraceConstants.File_Extension_json)) {
-            System.out.println(WordTraceConstants.Error_InvalidFileType);
-            return;
-        }
-        System.out.println(WordTraceConstants.Message_FilePathCorrect);
-        System.out.println(WordTraceConstants.Message_Searching+" "+ searchWord);
-        System.out.println(WordTraceConstants.Message_Processing);
+        System.out.println(WordTraceConstants.MESSAGE_INPUT_FILEPATH + inputFilePath);
+        System.out.println(WordTraceConstants.MESSAGE_SEARCH_WORD + searchWord);
+        System.out.println(WordTraceConstants.MESSAGE_PROCESSING);
     }
 }
---
+++ src/main/java/com/susmitha/wordtraceconstants/WordTraceConstants.java
@@ -0,0 +1,13 @@
+package com.susmitha.wordtraceconstants;
+public class WordTraceConstants {
+    public static final String ERROR_INSUFFICIENT_ARGUMENTS = "Please provide both the file path and the word to search";
+    public static final String ERROR_MORE_ARGUMENTS = "Please provide only the file path and the word to be searched";
+    public static final String ERROR_INVALID_FILE = "File path is incorrect or file doesnot exist";
+    public static final String FILE_EXTENSION_TXT = ".txt";
+    public static final String FILE_EXTENSION_JSON = ".json";
+    public static final String MESSAGE_INPUT_FILEPATH = "File Path : ";
+    public static final String MESSAGE_SEARCH_WORD = "Word to search : ";
+    public static final String MESSAGE_FILEPATH_CORRECT = "File path is correct!";
+    public static final String MESSAGE_SEARCHING = "Searching for word ?";
+    public static final String MESSAGE_PROCESSING = "Processing....";
+}
