1a6a35a75c7d8842d948811657c7313589da6570
susmitha.mereddy <mereddysusmitha2002@gmail.com>
2025-03-20 10:43:35 +0530

    Added validations

--- src/main/java/com/susmitha/wordtrace/WordTraceApp.java
+++ src/main/java/com/susmitha/wordtrace/WordTraceApp.java
@@ -1,7 +1,10 @@
 package com.susmitha.wordtrace;
 
 import constants.wordtraceconstants.WordTraceConstants;
+import fileprocessing.WordOccurrenceCounter;
 import validator.InputValidator;
+
+import java.io.File;
 
 /**
  * Main entry point for the WordTrace Application
@@ -20,11 +23,22 @@ public class WordTraceApp {
         }
         String inputFilePath = args[0];
         String searchWord = args[1];
+        //Creating object for File class
+        File inputFile = new File(inputFilePath);
+        String inputFileName = inputFile.getName();
         System.out.println(
                 WordTraceConstants.MESSAGE_INPUT_FILEPATH + inputFilePath + "\n" +
                         WordTraceConstants.MESSAGE_SEARCH_WORD + searchWord + "\n" +
                         WordTraceConstants.MESSAGE_FILEPATH_CORRECT + "\n" +
                         WordTraceConstants.MESSAGE_PROCESSING
         );
+        //Creating object for wordCounter class
+        WordOccurrenceCounter wordOccurrenceCounter = new WordOccurrenceCounter();
+        int count = wordOccurrenceCounter.countWordOccurrences(inputFilePath, searchWord);
+        if (count > 0) {
+            System.out.printf(String.format(WordTraceConstants.MESSAGE_COUNT_WORD, searchWord, count, inputFileName));
+        } else {
+            System.out.printf(String.format(WordTraceConstants.KEY_WORD_NOT_FOUND, searchWord, inputFileName));
+        }
     }
 }
